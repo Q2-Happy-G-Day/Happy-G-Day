3af69e8dee926d45e9e2bd4ae5c4a559b09bf093
Matthew Drumheller <MattDrummy@gmail.com>
2017-08-03 15:03:09 -0600

    added cors, pg, knex, and dotenv

--- app.js
+++ app.js
@@ -6,9 +6,10 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
-var users = require('./routes/users');
 
 var app = express();
+
+const cors = require('cors');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -21,9 +22,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
 
-app.use('/', index);
-app.use('/users', users);
+app.use('/index', index);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ knexfile.js
@@ -0,0 +1,13 @@
+// Update with your config settings.
+require('dotenv').config();
+
+module.exports = {
+
+  development: {
+    client: 'pg',
+    connection: 'postgres://localhost/gday'
+  },
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL + '?ssl=true'
+};
