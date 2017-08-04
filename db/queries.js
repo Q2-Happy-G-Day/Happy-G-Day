d90f86ffc85eaec6179bee2bfb4d1443608e9c3c
Matthew Drumheller <MattDrummy@gmail.com>
2017-08-04 12:34:34 -0600

    added query functions to queries.js

--- db/queries.js
+++ db/queries.js
@@ -1,5 +1,28 @@
 const knex = require('../db/knex');
 
 module.exports = {
-  
+  getAllUsers: () => {
+    return knex('user');
+  },
+  getOneUser: (id) => {
+    return knex('user').where('id', id)
+  },
+  getAllComments: () => {
+    return knex('comment');
+  },
+  getOneComment: (id) => {
+    return knex('comment').where('id', id);
+  },
+  postNewUser: (user) => {
+    return knex('user').insert(user).returning('*');
+  },
+  postNewComment: (comment) => {
+    return knex('comment').insert(comment).returning('*');
+  },
+  updateUser: (user, id) => {
+    return knex('user').where('id', id).update(user).returning('*');
+  },
+  deleteComment: (comment, id) => {
+    return knex('comment').where('id', id).del();
+  }
 }
