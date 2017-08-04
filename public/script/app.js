ab9f4166e4f39c8bcb6b71d983ebfe237c313d0c
Recardo Poole <recardo.poole@gmail.com>
2017-08-04 14:58:31 -0600

    updated modals nav into seperate files

--- public/script/app.js
+++ public/script/app.js
@@ -1,12 +1,20 @@
 var url = '/api/v1/users';
 
 $(document).ready(() => {
+  $('#nav').append(nav);
+  $('#modal').append(modal);
+
   $.get(url)
     .then((data) => {
 
       $("#myBtn1").click(function(event) {
-        event.preventDefault()
+        event.preventDefault();
         $("#myModal").modal();
+      })
+
+      $("#myBtn2").click(function(event) {
+        event.preventDefault();
+        $("#myModal2").modal();
       });
-    })
+    });
 });
---
+++ public/script/nav.js
@@ -0,0 +1,15 @@
+var nav = `<nav class="navbar navbar-toggleable-md navbar-light bg-faded header-logo">
+            <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <img class="navbar-nav mr-auto" src="./images/GDay Logo.png" width="200px" height="80px" alt="">
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+
+              <form class="form-inline my-2 my-lg-0 login-form">
+
+                <button id="myBtn1" class="btn btn-outline-success my-2 my-sm-0" type="submit">Sign up</button>
+                <button id="myBtn2" class="btn btn-outline-success my-2 my-sm-0" type="submit">Login</button>
+              </form>
+            </div>
+          </nav>`
