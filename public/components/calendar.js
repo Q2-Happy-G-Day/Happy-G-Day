f816fea2e0a14f5bb3de99e3b84d636188c8e6cf
Recardo Poole <recardo.poole@gmail.com>
2017-08-05 15:48:26 -0600

    finished Calander function

---
+++ public/components/calendar.js
@@ -0,0 +1,18 @@
+function calendar(users) {
+  for (var i = 1; i <= 12; i++) {
+    let months =`<div id="calenderBox">
+                  <div class="content">
+                    <div class="profiledate m${i}">
+                    </div>
+                    <div id="person${i}" class="persons">
+                    </div>
+                  </div>
+                </div>`
+    $('#calendar').append(months)
+    users.forEach((e) => {
+      if (e.birthday.slice(0, 2) == i) {
+        $(`#person${i}`).append(`<p>• ${e.birthday.slice(3, 5)} - ${e.name}</p>`)
+      }
+    })
+  }
+}
