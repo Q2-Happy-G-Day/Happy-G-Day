568b3ff0925ad8c714028e4af80c3edea0fb61de
Jasmine Cervantes <jt24cervantes@yahoo.com>
2017-08-07 14:46:13 -0600

    token

---
+++ routes/web-token.js
@@ -0,0 +1,21 @@
+const jwt = require('jsonwebtoken');
+const express = require('express');
+const knex = require('../db/knex');
+
+module.exports = function(req, res) {
+  knex('user')
+  .select('user.name', 'user.birthday', 'user.id')
+  .where('id', req.params.id)
+  .first()
+  .then(data => {
+    var token = jwt.sign(data, process.env.TOKEN_SECRET)
+    res.json({
+      token: token,
+      user: {
+      id: data.id,
+      name: data.name,
+      birthday: data.birthday
+      }
+    })
+  })
+}
