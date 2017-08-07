1b0d5de54bac9e6f6f31d5abf06a7955572e541b
Matthew Drumheller <MattDrummy@gmail.com>
2017-08-06 18:01:04 -0600

    merged Rick

--- public/components/nav.js
+++ public/components/nav.js
@@ -2,8 +2,7 @@ const nav = `<nav class="navbar navbar-toggleable-md navbar-light bg-faded heade
             <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
               <span class="navbar-toggler-icon"></span>
             </button>
-            <img class="navbar-nav mr-auto" src="./images/GDay Logo.png" width="200px" height="80px" alt="">
-
+              <img class="navbar-nav mr-auto" src="./images/GDay Logo.png" width="200px" height="80px" alt="">
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
 
               <form class="form-inline my-2 my-lg-0 login-form">
