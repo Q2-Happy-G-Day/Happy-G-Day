897e667687d51b1efc7527c373c4a3d1d721c00e
Jose Julio Figueiredo Junior <josejuliojunior@hotmail.com>
2017-08-07 16:02:08 -0600

    usersTime function

--- public/script/get-comments.js
+++ public/script/get-comments.js
@@ -1,6 +1,5 @@
 function getComments(user) {
   $.get(url + '/comments/' + user.id).then((comments) => {
-    console.log(comments);
     let userComment = JSON.parse(localStorage.getItem('currUser'))
     if (userComment) {
       profile(userComment, user, comments)
